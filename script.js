2e9a5630a3fbfb64999d5645edd3e5953f31a8e8
Sean <smarshallean@gmail.com>
2022-05-04 19:50:34 +1000

    Fixed size of etch-sketch so its fits the screen and added basic hover feature for grid squares

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
  function createGridSquares() {
     let gSContainer = document.getElementById("grid-square-container");
 
-    // Create 16 divs
-    for (let i = 0; i < 36; i++) {
+    // Create 16x16 divs
+    for (let i = 0; i < 256; i++) {
         let gridSquare = document.createElement("div");
         gridSquare.classList.add('grid-square');
         gSContainer.appendChild(gridSquare);
     }
 
-   
-    console.log("16 grid squares created");
 }
 
 createGridSquares();
