034fda35dd37b4dda46ec335a376dfbf66214aee
Sean <smarshallean@gmail.com>
2022-05-10 01:14:40 +1000

    etch-a-sketch is properly functional

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
- function createGridSquares() {
-    let gSContainer = document.getElementById("grid-square-container");
+let gSContainer = document.getElementById("grid-square-container");
 
-    // Create 16x16 divs
-    for (let i = 0; i < 256; i++) {
+function createGridSquares(number) {
+    for (let i = 0; i < (number * number); i++) {
         let gridSquare = document.createElement("div");
         gridSquare.classList.add('grid-square');
+        gridSquare.style.cssText = `width:calc(100%/${number + 1}); border: 2px solid lightgray;`
         gSContainer.appendChild(gridSquare);
     }
-
 }
 
-createGridSquares();
+createGridSquares(16);
+
+let mouseDown = false;
+gSContainer.onmousedown = () => (mouseDown = true);
+gSContainer.onmouseup = () => (mouseDown = false);
+
+let gridSquares = document.querySelectorAll('.grid-square');
+gridSquares.forEach(gridSquare => {
+    gridSquare.addEventListener("mousedown", () => {
+        gridSquare.classList.add('colored');
+    });
+
+    gridSquare.addEventListener("mouseover", () => {
+        if (mouseDown == true) {
+            gridSquare.classList.add('colored');
+        }
+    });
+});
+
