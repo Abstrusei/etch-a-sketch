00da5a7931f86b3f27d107b3b269a8fb439eb46c
Sean <smarshallean@gmail.com>
2022-05-11 01:22:51 +1000

    Fixed issues with 'eraser' button not disabling

--- script.js
+++ script.js
@@ -1,51 +1,88 @@
 const gSContainer = document.getElementById("grid-square-container");
 const sliderCaption = document.getElementById("slider-caption");
+let numSquaresPerSide = 16;
+let eraserOn = false;
 
 function generateGrid(number) {
+    if (gSContainer.innerHTML != "") {
+        removeGrid();
+    }
+
     for (let i = 0; i < (number * number); i++) {
         let gridSquare = document.createElement("div");
         gridSquare.classList.add('grid-square');
         gridSquare.style.cssText = `border: 1px solid lightgray; height: calc(100%/${number}); display: inline-block;`
         gSContainer.appendChild(gridSquare);
     }
-    addColoringFunctionality();
+    
+    turnEraserOff()
+    enableColoring();
     sliderCaption.innerHTML = `${number}`
+    numSquaresPerSide = number; //Set number of squares per side in case of 'clear'
 }
 
-generateGrid(16);
+generateGrid(numSquaresPerSide);
 
-function addColoringFunctionality() {
-    let mouseDown = false;
-    gSContainer.onmousedown = () => (mouseDown = true);
-    gSContainer.onmouseup = () => (mouseDown = false);
+let mouseDown = false;
+gSContainer.onmousedown = () => (mouseDown = true);
+gSContainer.onmouseup = () => (mouseDown = false);
 
+function enableColoring() {
     const gridSquares = document.querySelectorAll('.grid-square');
     gridSquares.forEach(gridSquare => {
         gridSquare.addEventListener("mousedown", () => {
-            gridSquare.classList.add('colored');
+            if (eraserOn && gridSquare.classList.contains('colored')) {
+                gridSquare.classList.remove('colored');
+            } else {
+                gridSquare.classList.add('colored');
+            }
         });
 
         gridSquare.addEventListener("mouseover", () => {
             if (mouseDown == true) {
-                gridSquare.classList.add('colored');
+                if (eraserOn && gridSquare.classList.contains('colored')) {
+                    gridSquare.classList.remove('colored');
+                } else {
+                    gridSquare.classList.add('colored');
+                }
             }
         });
     });
 }
 
 function removeGrid() {
-    const gridSquares = document.querySelectorAll('.grid-square');
-    gridSquares.forEach(gridSquare => {
-        gSContainer.removeChild(gridSquare);
-    });
+    gSContainer.innerHTML = "";
 }
 
-// Clear Button
+function turnEraserOff() {
+    if (eraserOn == true) {
+        eraserBtn.classList.remove('eraser-on');
+        eraserOn = false;
+    }
+}
 
+ 
 // Eraser Button
+const eraserBtn = document.getElementById('eraser-btn');
+eraserBtn.addEventListener("click", () => {
+    if (eraserOn == true) {
+        eraserBtn.classList.remove('eraser-on');
+        eraserOn = false;
+    } else {
+        eraserBtn.classList.add('eraser-on');
+        eraserOn = true;
+    }
+})
+
+// Clear Button
+const clearBtn = document.getElementById('clear-btn');
+clearBtn.addEventListener("click", () => {
+    generateGrid(numSquaresPerSide);
+    turnEraserOff();
+})
 
 // Reset Button
-const resetBtn = document.getElementById('clear-btn');
+const resetBtn = document.getElementById('reset-btn');
 resetBtn.addEventListener("click", () => {
     window.location.reload();
 })
@@ -58,7 +95,6 @@ rangeSlider.addEventListener("input", () => {
 });
 
 rangeSlider.addEventListener("change", () => {
-    removeGrid();
     generateGrid(rangeSlider.value);
 });
 
